bb5325c92540e302f55d2d71a83041a1f78efd61
thabrita <britothais30@gmail.com>
2022-12-01 22:00:56 -0300

    Adicionando conteúdo nas páginas

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,10 +1,19 @@
 import { Header } from "../components/Header";
 import contactImg from '../assets/contact.svg';
+import styles from '../styles/pages/contato.module.css';
 
 export function Contato() {
     return (
         <>
         <Header text="Entre em contato" image={contactImg}/>
+        <div>
+            <form className={styles.form}>
+                <input placeholder="Digite seu nome" className={styles.formInput}/>
+                <input placeholder="Digite seu email" className={styles.formInput}/>
+                <textarea placeholder="Digite sua mensagem" className={styles.formTextArea}/>
+                <button className={styles.formButton}>Enviar mensagem</button>
+            </form>
+        </div>
         </>
     )
 }
